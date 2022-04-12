adc0618b8667aa5bff035cb28132842f01b76fd3
textcoder <88121662+textcoder@users.noreply.github.com>
2022-04-12 20:50:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	
+    fabric.Image.fromURL('birthdayImage.jpg',function(Img){
+
+        block_image_object = Img;
+        
+        block_image_object.scaleToWidth(800);
+        block_image_object.scaleToWidth(510);
+        block_image_object.set({
+            top:0, 
+            left:0
+        });
+        canvas.add(block_image_object);
+    });
+	
+}
+
+function playSound(){
+	x.play();
+}
